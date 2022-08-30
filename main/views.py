60945a26f48131e4dc0ebe5661dabe568614a27d
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-30 21:40:14 +0000

    wrote out both fbv and cbv for contact page, learnt more about forms

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views.generic.edit import FormView
+from main import forms
 
 
 def home(request):
@@ -7,3 +9,24 @@ def home(request):
 
 def about(request):
     return render(request, 'main/about.html', {})
+
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = forms.ContactUsForm(request.POST)
+        if form.is_valid():
+            form.send_mail()
+            return redirect(reverse('main:home'))
+    else:
+        form = forms.ContactUsForm()
+    return render(request, 'main/contact.html', {'form': form})
+
+
+class ContactUsView(FormView):
+    template_name = 'main/contact.html'
+    form_class = forms.ContactUsForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
