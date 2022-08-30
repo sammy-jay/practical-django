a41a9ebe77b0564027e320d271504ff25cde30d8
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-30 19:42:33 +0000

    added page tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get(reverse('main:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/home.html')
+        self.assertContains(response, 'Home Page')
+
+    def test_about_page_works(self):
+        response = self.client.get(reverse('main:about'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/about.html')
+        self.assertContains(response, 'About Page')
