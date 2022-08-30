b2c605b44e072392586e9fe98e14e8d222657045
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-30 21:39:34 +0000

    added contact url

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('contact-us/', views.contact_us, name='contact-us'),
+    # path('contact-us/', views.ContactUsView.as_view(), name='contact-us'),
 ]
