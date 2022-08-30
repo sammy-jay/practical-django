ea748c80cfaaa19c59ffe8af5bc96cf290c36b14
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-30 18:46:20 +0000

    project initialization and basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
